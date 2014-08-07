1baf96a78d27d1a03cf98d533de0565bb02831bd
xiaozhujun <xiaozhujun520@163.com>
2014-08-07 10:19:37 +0800

    删除临时生成文件

--- gen/org/whut/inspectplatform/BuildConfig.java
+++
@@ -1,8 +0,0 @@
-/*___Generated_by_IDEA___*/
-
-/** Automatically generated file. DO NOT MODIFY */
-package org.whut.inspectplatform;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
